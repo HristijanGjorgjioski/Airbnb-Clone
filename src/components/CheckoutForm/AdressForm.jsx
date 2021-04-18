5a2f848e22c0b612de0a4f4acb528e8663e9df69
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-04-18 08:09:51 +0200

    Trying to work with hooks form

--- src/components/CheckoutForm/AdressForm.jsx
+++ src/components/CheckoutForm/AdressForm.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+
+import FormInput from './CustomTextField';
 
 const AdressForm = () => {
+    const methods = useForm();
+
     return (
-        <div>
-            AdressForm
-        </div>
+        <>
+            <Typography variant="h6" gutterBottom>Shipping Adress</Typography>
+            <FormProvider {...methods}>
+                <form onSubmit=''>
+                    <Grid container spacing={3}>
+                        <FormInput required name='firstName' label='First Name' />
+                    </Grid>
+                </form>
+            </FormProvider>
+        </>
     )
 }
 
